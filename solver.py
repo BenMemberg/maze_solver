96b82fa77c6630af8b1f5436a29848c3928f45bc
Ben Memberg <benmemberg@gmail.com>
2022-01-21 15:27:20 -0500

    Move find_starting_coordinates to the MazeAgent class
    The find_starting_coordinates method was added before the MazeAgent class was
    created. But its functionality really belongs in the MazeAgent class, as the
    class should be able to simply take a matrix of maze data and process it.
    
    This commit moves the method to the MazeAgent class. It also updates the
    MazeAgent __init__ to call self.find_starting_coordinates() to get the maze
    start coordinates instead of taking them as an argument.
    
    This commit also adds a maze_start property to the MazeAgent class that returns
    the starting coordinates as a tuple for comparison/display purposes.

--- solver.py
+++ solver.py
@@ -19,31 +19,39 @@ def load_maze(maze_file):
             maze.append([int(i) for i in row])
     return maze
 
-def find_starting_coordinates(maze):
-    """
-    Finds the coordinate of the entrance to the maze. If there is more than
-    one entrance, returns the first entry coordinates found.
-    """
-    entry_row = maze[0]
-    for point, val in enumerate(entry_row):
-        if int(val)==0:
-            return (point, 0)
-    raise ValueError("Entry not found! Entry row given:\n" + format_row(entry_row))
-
 
 class MazeAgent(object):
     """
     A class that holds the data and methods needed for an agent to navigate a
     given maze.
     """
-    def __init__(self, maze, maze_start):
+    def __init__(self, maze):
         # Initialize maze data from args
         self.maze = maze
-        self.start_row, self.start_col = maze_start
+        self.start_row, self.start_col = self.find_starting_coordinates()
 
         # Initialize agent's route matrix
         self.routes = self.zeros_matrix(len(maze[0]), len(maze))
         self.routes[self.start_col][self.start_row] = 1
+
+    @property
+    def maze_start(self):
+        """
+        Returns the coordinates of the maze start as a tuple.
+        """
+        return (self.start_row, self.start_col)
+
+    def find_starting_coordinates(self):
+        """
+        Finds the coordinate of the entrance to the maze. If there is more than
+        one entrance, returns the first entry coordinates found.
+        """
+        entry_row = self.maze[0]
+        for point, val in enumerate(entry_row):
+            if val == 0:
+                return (point, 0)
+        raise ValueError(
+            "Entry not found! Entry row given:\n" + format_row(entry_row))
 
     def zeros_matrix(self, width, length):
         """
@@ -71,7 +79,5 @@ if __name__ == '__main__':
     maze = load_maze(args.maze_file)
     print("Maze Provided:\n" + format_maze(maze))
 
-    maze_start = find_starting_coordinates(maze)
-    print("Maze Entry: " + str(maze_start))
-
-    maze_agent = MazeAgent(maze, maze_start)
+    maze_agent = MazeAgent(maze)
+    print(f"Maze Entry: {maze_agent.maze_start}")
