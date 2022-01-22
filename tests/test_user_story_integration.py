f1a0b5185f104755508f9c96fc7486ed4be19bb4
Ben Memberg <benmemberg@gmail.com>
2022-01-21 22:59:24 -0500

    Extend integration tests to check solution path
    This commit adds checks to the integration tests to ensure that the solution
    path is what we expect.

--- tests/test_user_story_integration.py
+++ tests/test_user_story_integration.py
@@ -26,6 +26,11 @@ class TestUserStories(unittest.TestCase):
         maze_exit = maze_agent.find_exit()
         self.assertEqual(maze_exit, (1, 0), "Maze Exit should be (1, 0)")
 
+        # Ensure the maze path is correct
+        expected_path = [(1, 0)]
+        self.assertEqual(
+            maze_agent.maze_path, expected_path, "Wrong maze path!")
+
     def test_user_story_2(self):
         # Test loading in the maze
         expected_maze = [
@@ -47,6 +52,11 @@ class TestUserStories(unittest.TestCase):
         maze_exit = maze_agent.find_exit()
         self.assertEqual(maze_exit, (2, 3), "Maze Exit should be (2, 3)")
 
+        # Ensure the maze path is correct
+        expected_path = [(2, 0), (2, 1), (2, 2), (2, 3)]
+        self.assertEqual(
+            maze_agent.maze_path, expected_path, "Wrong maze path!")
+
     def test_user_story_3(self):
         # Test loading in the maze
         expected_maze = [
@@ -67,6 +77,11 @@ class TestUserStories(unittest.TestCase):
         # Use the maze agent to find the maze exit coordinates
         maze_exit = maze_agent.find_exit()
         self.assertEqual(maze_exit, (3, 3), "Maze Exit should be (3, 3)")
+
+        # Ensure the maze path is correct
+        expected_path = [(1, 0), (1, 1), (1, 2), (2, 2), (3, 2), (3, 3)]
+        self.assertEqual(
+            maze_agent.maze_path, expected_path, "Wrong maze path!")
 
     def test_user_story_4(self):
         # Test loading in the maze
@@ -92,6 +107,14 @@ class TestUserStories(unittest.TestCase):
         maze_exit = maze_agent.find_exit()
         self.assertEqual(maze_exit, (3, 6), "Maze Exit should be (3, 6)")
 
+        # Ensure the maze path is correct
+        expected_path = [
+            (1, 0), (1, 1), (2, 1), (3, 1), (3, 2), (3, 3), (2, 3),
+            (1, 3), (1, 4), (1, 5), (2, 5), (3, 5), (3, 6)
+        ]
+        self.assertEqual(
+            maze_agent.maze_path, expected_path, "Wrong maze path!")
+
     def test_user_story_5(self):
         # Test loading in the maze
         expected_maze = [
@@ -115,6 +138,14 @@ class TestUserStories(unittest.TestCase):
         # Use the maze agent to find the maze exit coordinates
         maze_exit = maze_agent.find_exit()
         self.assertEqual(maze_exit, (3, 5), "Maze Exit should be (3, 5)")
+
+        # Ensure the maze path is correct
+        expected_path = [
+            (1, 0), (1, 1), (1, 2), (1, 3),
+            (2, 3), (3, 3), (3, 4), (3, 5)
+        ]
+        self.assertEqual(
+            maze_agent.maze_path, expected_path, "Wrong maze path!")
 
     def test_full_maze(self):
         # Test loading in the maze
@@ -142,6 +173,15 @@ class TestUserStories(unittest.TestCase):
         # Use the maze agent to find the maze exit coordinates
         maze_exit = maze_agent.find_exit()
         self.assertEqual(maze_exit, (7, 9), "Maze Exit should be (7, 9)")
+
+        # Ensure the maze path is correct
+        expected_path = [
+            (4, 0), (4, 1), (5, 1), (5, 2), (5, 3), (4, 3), (3, 3), (3, 4),
+            (3, 5), (4, 5), (5, 5), (5, 6), (5, 7), (5, 8), (6, 8), (7, 8),
+            (7, 9)
+        ]
+        self.assertEqual(
+            maze_agent.maze_path, expected_path, "Wrong maze path!")
 
 
 if __name__ == '__main__':
