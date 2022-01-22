fbecf4f444138c5d030046cb81e5469aab982e18
Ben Memberg <benmemberg@gmail.com>
2022-01-22 12:41:06 -0500

    Add MazeAgent.zeros_matrix tests
    This commit adds two tests for the MazeAgent.zeros_matrix method. The first
    tests a small non-symmetrical matrix while the second tests a matrix the size
    of a full maze.

--- tests/test_maze_agent.py
+++ tests/test_maze_agent.py
@@ -23,3 +23,38 @@ class TestMazeAgent(unittest.TestCase):
         ]
         self.maze_agent = MazeAgent(self.maze)
         self.step = 1
+
+    def test_zeros_matrix_short(self):
+        # Test zeros_matrix method with a non-symmetrical dimension of 4x3
+        expected_zeros_matrix_short = [
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+        ]
+        zeros_matrix_short = self.maze_agent.zeros_matrix(4, 3)
+        self.assertEqual(
+            zeros_matrix_short,
+            expected_zeros_matrix_short,
+            "Zeros matrix genererated incorrectly!")
+
+    def test_zeros_matrix_short_full(self):
+        # Test zeros_matrix method with a full size matrix (len/width of
+        # self.maze).
+        expected_zeros_matrix_full = [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        ]
+        zeros_matrix_full = self.maze_agent.zeros_matrix(
+                                len(self.maze[0]), len(self.maze))
+        self.assertEqual(
+            zeros_matrix_full,
+            expected_zeros_matrix_full,
+            "Zeros matrix genererated incorrectly!")
