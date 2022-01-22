defc475491a5d3986b2d1c51955a3049e5424dbf
Ben Memberg <benmemberg@gmail.com>
2022-01-22 13:24:41 -0500

    Add TestUtils tests for format_row and format_maze
    This commit adds tests for display of simple rows/matrices with no solution
    path data.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -21,3 +21,23 @@ class TestUtils(unittest.TestCase):
             [1, 1, 0, 1, 1, 0, 0, 0, 0, 1],
             [1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
         ]
+
+    def test_format_row(self):
+        expected_output = " #     #  #  #     #  #  #  # "
+        formatted_row = format_row(self.maze[6])
+        self.assertEqual(formatted_row, expected_output)
+
+    def test_format_maze_no_path(self):
+        formatted_maze = format_maze(self.maze)
+        expected_output = (
+            " #  #  #  #     #  #  #  #  # \n"
+            " #                          # \n"
+            " #     #  #  #     #  #     # \n"
+            " #  #  #              #     # \n"
+            " #           #  #  #  #     # \n"
+            " #  #  #           #        # \n"
+            " #     #  #  #     #  #  #  # \n"
+            " #                 #  #     # \n"
+            " #  #     #  #              # \n"
+            " #  #  #  #  #  #  #     #  # \n")
+        self.assertEqual(formatted_maze, expected_output)
