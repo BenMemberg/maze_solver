83218af59ac7ceddfbaf7953406d50473c6ac434
Ben Memberg <benmemberg@gmail.com>
2022-01-22 13:22:36 -0500

    Add TestUtils unittest class to testing fixture
    This commit adds the class TestUtils to the unittest testing fixture. Tests in
    this class will test the utility display methods in utils.py.

---
+++ tests/test_utils.py
@@ -0,0 +1,23 @@
+# tests/test_utils.py
+import unittest
+
+from utils import format_row, format_maze
+
+
+class TestUtils(unittest.TestCase):
+    """
+    This class tests the utility display methods in utils.py.
+    """
+    def setUp(self) -> None:
+        self.maze = [
+            [1, 1, 1, 1, 0, 1, 1, 1, 1, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 1, 1, 1, 0, 1, 1, 0, 1],
+            [1, 1, 1, 0, 0, 0, 0, 1, 0, 1],
+            [1, 0, 0, 0, 1, 1, 1, 1, 0, 1],
+            [1, 1, 1, 0, 0, 0, 1, 0, 0, 1],
+            [1, 0, 1, 1, 1, 0, 1, 1, 1, 1],
+            [1, 0, 0, 0, 0, 0, 1, 1, 0, 1],
+            [1, 1, 0, 1, 1, 0, 0, 0, 0, 1],
+            [1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
+        ]
