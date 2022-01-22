d5017d97659f409fbb1d4ede0cae54f216c552b0
Ben Memberg <benmemberg@gmail.com>
2022-01-22 12:37:35 -0500

    Add TestMazeAgent unittest class
    This commit adds the unittest class for testing the instance methods of
    the MazeAgent class.

---
+++ tests/test_maze_agent.py
@@ -0,0 +1,25 @@
+# tests/test_maze_agent.py
+import unittest
+
+from agent import MazeAgent
+
+
+class TestMazeAgent(unittest.TestCase):
+    """
+    This class tests the supporting instance methods of the MazeAgent class.
+    """
+    def setUp(self):
+        self.maze = [
+            [1, 1, 1, 1, 0, 1, 1, 1, 1, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 1, 1, 1, 0, 1, 1, 0, 1],
+            [1, 1, 1, 0, 0, 0, 0, 1, 0, 1],
+            [1, 0, 0, 0, 1, 1, 1, 1, 0, 1],
+            [1, 1, 1, 0, 0, 0, 1, 0, 0, 1],
+            [1, 0, 1, 1, 1, 0, 1, 1, 1, 1],
+            [1, 0, 0, 0, 0, 0, 1, 1, 0, 1],
+            [1, 1, 0, 1, 1, 0, 0, 0, 0, 1],
+            [1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
+        ]
+        self.maze_agent = MazeAgent(self.maze)
+        self.step = 1
