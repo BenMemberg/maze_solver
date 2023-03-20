faf77a489c65744e18061962c7eee2dd22e0e806
Ben Memberg <benmemberg@gmail.com>
2023-03-19 21:58:13 -0400

    Add the print_maze utils method
    This method is useful for printing a maze during of after maze generation

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 # utils.py
 
+from colorama import  Fore, init
+from constants import WALL, OPEN, UNVISITED
 
 def format_row(row):
     """
@@ -31,3 +33,24 @@ def format_maze(maze, maze_path=[]):
         maze_str += row_str
         maze_str += "\n"
     return maze_str
+
+def print_maze(maze):
+    """
+    Prints out a given matrix in colored ASCII format. Can be used before, during, or
+    after maze generation.
+    """
+    init()
+    for i in range(0, len(maze)):
+        for j in range(0, len(maze[i])):
+            if (maze[i][j] == UNVISITED):
+                print(Fore.WHITE + 'U', end="  ")
+            elif (maze[i][j] == OPEN):
+                # print(Fore.GREEN + ' ', end="  ")
+                print(Fore.BLACK + '0', end="  ")
+                # print(Fore.GREEN + 'O', end="  ")
+            elif (maze[i][j] == WALL):
+                print(Fore.RED + '#', end="  ")
+            else:
+                print(Fore.MAGENTA + str(maze[i][j]), end="  ")
+            
+        print(Fore.WHITE + '\n')
